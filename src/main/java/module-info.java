04b2e68d2719f8e70fd42886c171ffb2f2a295fa
Benedict <119060277+be-edict@users.noreply.github.com>
2024-11-28 22:49:00 +0100

    Delete src/main/java/module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module de.osfalia.reinhard.pixeldrawtest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens de.osfalia.reinhard.pixeldrawtest to javafx.fxml;
-    exports de.osfalia.reinhard.pixeldrawtest;
-}
